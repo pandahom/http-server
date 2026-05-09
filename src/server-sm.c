34146766110dcfdfd696d7fefdedeb2ace4ff7d4
nim <nimaeraghi98@gmail.com>
2026-05-09 23:50:53 +0330

    feat: main server loop,

---
+++ src/server-sm.c
@@ -0,0 +1,63 @@
+#include "server.h"
+#include <pthread.h>
+
+const srv_state_e transition_table[SRV_STATE_COUNT][SRV_EVENT_COUNT] = {
+    [SRV_STATE_INIT] = {
+        [SRV_EVENT_CONSTRUCTED] = SRV_STATE_LISTENING
+    },
+
+    [SRV_STATE_LISTENING] = {
+        [SRV_EVENT_CONNECTION_RECEIVED] = SRV_STATE_ACCEPTED,
+    },
+    [SRV_STATE_ACCEPTED] = {
+            [SRV_EVENT_RESET] = SRV_STATE_LISTENING,
+    }
+};
+
+int on_error_server(server_ctx_t *server) {
+    close(server->fd);
+    printf("Terminating Program ....\n");
+    return FAIL;
+}
+
+int handle_srv_states(void) {
+    int ret = OK;
+    server_ctx_t server   = {
+            .sm = { .current_state = SRV_STATE_INIT},
+            .backlog = 1
+    };
+    srv_sm_t *server_sm = &server.sm;
+    client_ctx_t *new_con  = NULL;
+    pthread_t new_th;
+
+
+    char *ip_address = "127.0.0.4";
+    uint16_t port    = 8080;
+
+    while (true) {
+        switch (server_sm->current_state) {
+            case SRV_STATE_INIT:
+                construct_server(&server, port, ip_address, server.backlog);
+                break;
+            case SRV_STATE_LISTENING:
+                new_con = accept_connection(&server);
+                break;
+            case SRV_STATE_ACCEPTED:
+                new_con->sm.current_state = CONN_STATE_ACCEPTED;
+                server_sm->event_trigger = SRV_EVENT_RESET;
+                pthread_create(&new_th, NULL, handle_conn_states, new_con);
+                pthread_detach(new_th);
+                break;
+            case SRV_STATE_ERROR:
+                // TODO: Handle Threads on error too
+                ret = on_error_server(&server);
+                free(new_con);
+                goto done;
+            default:
+                break;
+        }
+        state_transit(server_sm);
+    }
+done:
+    return ret;
+}
