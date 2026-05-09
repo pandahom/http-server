a31c860b8968ccc19cc18b6c148373dd8c3484dd
nim <nimaeraghi98@gmail.com>
2026-05-09 23:40:17 +0330

    feat: Structures needed for server's state machine.

---
+++ src/server-sm.h
@@ -0,0 +1,32 @@
+#ifndef SERVER_SM_H
+#define SERVER_SM_H
+
+typedef enum {
+    SRV_STATE_ERROR,
+    SRV_STATE_INIT,
+    SRV_STATE_LISTENING,
+    SRV_STATE_ACCEPTED,
+    SRV_STATE_COUNT
+} srv_state_e;
+
+typedef enum {
+    SRV_EVENT_ERROR,
+    SRV_EVENT_CONSTRUCTED,
+    SRV_EVENT_CONNECTION_RECEIVED,
+    SRV_EVENT_RESET,
+    SRV_EVENT_COUNT
+} srv_event_e;
+
+typedef struct {
+    srv_state_e current_state;
+    srv_event_e event_trigger;
+} srv_sm_t;
+
+extern const srv_state_e transition_table[SRV_STATE_COUNT][SRV_EVENT_COUNT];
+
+#define state_transit(sm_ptr)\
+    (sm_ptr)->current_state = transition_table[(sm_ptr)->current_state][(sm_ptr)->event_trigger]
+
+int handle_srv_states(void);
+
+#endif
